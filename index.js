6f52196edf642443c82735f930d2e35e906a1d5f
Mridul54 <dasmridul364@gmail.com>
2023-11-08 03:57:53 +0600

    added books data in database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,12 +28,73 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const bookCategoryCollection = client.db('bookCategory').collection('category');
+
+    const bookCollection = client.db('bookDB').collection('book');
+
+    app.get('/category', async(req, res) => {
+        const cursor = bookCategoryCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/category/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)}
+        const result = await bookCategoryCollection.findOne(query);
+        res.send(result);
+    })
+
+    
+
+    app.post('/book', async(req, res) => {
+        const newBook =req.body;
+        console.log(newBook);
+        const result = await bookCollection.insertOne(newBook);
+        res.send(result);
+    })
+
+    app.get('/book', async(req, res) => {
+        const cursor = bookCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/book/:id', async(req, res)=> {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await bookCollection.findOne(query);
+        res.send(result)
+    })
+
+    app.put('/book/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const options = { upsert: true};
+        const updatedBook = req.body;
+        const book = {
+            $set: {
+                photo: updatedBook.photo, 
+                name: updatedBook.name, 
+                quantity: updatedBook.quantity, 
+                author: updatedBook.author, 
+                category: updatedBook.category, 
+                description: updatedBook.description, 
+                rating: updatedBook.rating
+            }
+        }
+        const result = await bookCollection.updateOne(filter, book, options);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
